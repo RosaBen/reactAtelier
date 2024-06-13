f6cc4667295f040c6e6327d946658fca70ea1243
Rosa Benchabane <benchabane.rosa@outlook.com>
2024-06-13 11:46:02 +0200

    feat: state done

--- rosa_project/src/components/Avatar.jsx
+++ rosa_project/src/components/Avatar.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
 export default function Avatar ({character}){
 
+    const [count, setCount] = useState(0);
 
-    return  <figure>{character.image ? (<img src={character.image} alt={character.firstName}/>) : (<p>?????</p>)}<figcaption>{character.firstName} {character.lastName}</figcaption></figure>;
+    return  <figure>{character.image ? (<img src={character.image} alt={character.firstName}/>) : (<p>?????</p>)}<figcaption>{character.firstName} {character.lastName}     <button onClick={
+        ()=>setCount(count+1)
+    }><img src="../src\assets\donut.png" alt="donut" width={20}/></button> {count}</figcaption></figure>;
+
+
 }
