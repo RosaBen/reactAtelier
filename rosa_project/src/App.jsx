6a977445a3a6bf8937b329b0ef3b85e313385d8e
Rosa Benchabane <benchabane.rosa@outlook.com>
2024-06-13 11:17:31 +0200

    feat:props

--- rosa_project/src/App.jsx
+++ rosa_project/src/App.jsx
@@ -1,38 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Avatar from './components/Avatar'
 import './App.css'
-import Hello from './Hello'
+
+
+
+const characters = [
+  {
+      image: "https://fastly.picsum.photos/id/4/200/300.jpg?hmac=y6_DgDO4ccUuOHUJcEWirdjxlpPwMcEZo7fz1MpuaWg",
+      firstName: "Maggie",
+      lastName: "Simpson"
+  },
+  {
+      image: "https://picsum.photos/id/237/200/300",
+      firstName: "Bart",
+      lastName: "Simpson"
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        < Hello/>
-
-      {
-      
-      /* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+<Avatar character= {characters[0]}/>
+<Avatar character= {characters[1]}/>
     </>
   )
 }
